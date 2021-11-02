7a9ef76a6cd334f92adb383590c2ee28caa707fa
maxrchung <maxrchung@gmail.com>
2021-11-01 23:40:24 -0700

    Set up grid

--- src/main.js
+++ src/main.js
@@ -1,3 +1,20 @@
 "use strict";
-console.log('hello world');
+var width = 800;
+var lightGray = 'rgb(200, 200, 200)';
+var rows = 40;
+var rowWidth = width / rows;
+var canvas = document.getElementById('canvas');
+canvas.width = width;
+canvas.height = width;
+var context = canvas.getContext('2d');
+context.beginPath();
+for (var i = 0; i <= width; i += rowWidth) {
+    context.moveTo(i, 0);
+    context.lineTo(i, width);
+    context.moveTo(0, i);
+    context.lineTo(width, i);
+}
+context.closePath();
+context.strokeStyle = lightGray;
+context.stroke();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log('hello world')
+const width = 800;
+const lightGray = 'rgb(200, 200, 200)';
+const rows = 40;
+const rowWidth = width / rows;
+
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+canvas.width = width;
+canvas.height = width;
+
+const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+context.beginPath();
+for (let i = 0; i <= width; i += rowWidth) {
+    context.moveTo(i, 0);
+    context.lineTo(i, width);
+    context.moveTo(0, i);
+    context.lineTo(width, i);
+}
+context.closePath();
+context.strokeStyle = lightGray;
+context.stroke();
